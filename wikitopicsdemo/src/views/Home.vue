6c9057e9d3a93765efb1efe7debdc59929eac017
Moriel Schottlender <moriel@gmail.com>
2020-10-14 13:48:01 -0700

    Add a demo interface

---
+++ wikitopicsdemo/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="container">
+      </div>
+    <img alt="Vue logo" src="../assets/phoenix.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home'
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
