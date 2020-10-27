4fcb078d99223b6f72013146ff31a22fd657a3f8
Moriel Schottlender <moriel@gmail.com>
2020-10-27 15:16:02 -0700

    deploy

---
+++ precache-manifest.4469a3e24c67ce9ce888c42942f3b17f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0657efad40d75d5390",
+    "url": "/experiment-wp-section-topics/css/app.c577228c.css"
+  },
+  {
+    "revision": "e2da6ca7ec34343f3a07",
+    "url": "/experiment-wp-section-topics/css/chunk-vendors.3a25ac68.css"
+  },
+  {
+    "revision": "ad7078cdf0784744b2f7023aceee6df0",
+    "url": "/experiment-wp-section-topics/img/phoenix-white.ad7078cd.png"
+  },
+  {
+    "revision": "f4e4653c0d73232a4e65a8e837daf7a7",
+    "url": "/experiment-wp-section-topics/img/phoenix.f4e4653c.png"
+  },
+  {
+    "revision": "4302cd0df12b6d4f3559f2eea5e465c7",
+    "url": "/experiment-wp-section-topics/index.html"
+  },
+  {
+    "revision": "5f0657efad40d75d5390",
+    "url": "/experiment-wp-section-topics/js/app.dd384c9b.js"
+  },
+  {
+    "revision": "e2da6ca7ec34343f3a07",
+    "url": "/experiment-wp-section-topics/js/chunk-vendors.25fcde2c.js"
+  },
+  {
+    "revision": "1a98dd046cda4db2def0b16355dd62ae",
+    "url": "/experiment-wp-section-topics/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/experiment-wp-section-topics/robots.txt"
+  }
+]);
